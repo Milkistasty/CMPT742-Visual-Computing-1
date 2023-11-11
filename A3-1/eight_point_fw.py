7e3d92a56e9ae5c18a25d7525d55f8b2115d78d3
Milkistasty <wenhewangcrane@gmail.com>
2023-11-10 20:39:09 -0800

    A3, lab 2 and lab4 update

--- A3-1/eight_point_fw.py
+++ A3-1/eight_point_fw.py
@@ -46,28 +46,110 @@ def drawlines(img1,img2,lines,pts1,pts2):
         img2 = cv2.circle(img2,tuple(pt2),5,color,-1)
     return img1,img2
 
+def normalize_points(pts):
+    # Compute the centroid of the points
+    centroid = np.mean(pts, axis=0)
+    
+    # Shift the origin of the points to the centroid
+    shifted_pts = pts - centroid
+
+    # Compute the mean distance of all points from the centroid
+    mean_dist = np.mean(np.sqrt(np.sum(shifted_pts**2, axis=1)))
+
+    # Scale the mean distance to sqrt(2)
+    scale = np.sqrt(2) / mean_dist
+
+    # Construct the normalization matrix
+    T = np.array([[scale, 0, -scale * centroid[0]],
+                  [0, scale, -scale * centroid[1]],
+                  [0, 0, 1]])
+
+    # Normalize the points
+    pts = T @ np.vstack((pts.T, np.ones(pts.shape[0])))
+    return pts[0:2].T, T
+
 def FindFundamentalMatrix(pts1, pts2):
     #Input: two lists of corresponding keypoints (numpy arrays of shape (N, 2))
     #Output: fundamental matrix (numpy array of shape (3, 3))
 
+    """
+    The matrix settings and general structure refers to CMU slides
+    https://www.cs.cmu.edu/~16385/s17/Slides/12.4_8Point_Algorithm.pdf
+
+    The code refers to GaTech, Project 3: Camera Calibration and Fundamental Matrix Estimation with RANSAC
+    https://sites.cc.gatech.edu/classes/AY2016/cs4476_fall/results/proj3/html/tguo40/index.html
+    
+    """
+
     #todo: Normalize the points
+    pts1, T1 = normalize_points(pts1)
+    pts2, T2 = normalize_points(pts2)
 
     #todo: Form the matrix A
-
+    A = np.zeros((len(pts1), 9))
+    for i in range(len(pts1)):
+        x1, y1 = pts1[i]
+        x2, y2 = pts2[i]
+        A[i] = [x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1]
+        
     #todo: Find the fundamental matrix
-    raise NotImplementedError
+    # Find the SVD of (A^T A)
+    U, S, V = np.linalg.svd(A)
+    # Entries of F are the elements of column of V corresponding to the least singular value
+    F = V[-1].reshape(3, 3)
+
+    # Enforce rank 2 constraint on F
+    U, S, V = np.linalg.svd(F)
+    S[2] = 0
+    F = np.dot(U, np.dot(np.diag(S), V))
+
+    # Un-normalize F
+    F = np.dot(T2.T, np.dot(F, T1))
+
+    return F
 
 def FindFundamentalMatrixRansac(pts1, pts2, num_trials = 1000, threshold = 0.01):
     #Input: two lists of corresponding keypoints (numpy arrays of shape (N, 2))
     #Output: fundamental matrix (numpy array of shape (3, 3))
 
     #todo: Run RANSAC and find the best fundamental matrix
-    raise NotImplementedError
+    best_inliers_count = 0
+    best_fundamental_matrix = None
+
+    for _ in range(num_trials):
+        # Randomly sample 8 points
+        indices = np.random.choice(len(pts1), 8, replace=False)
+        sampled_pts1 = pts1[indices]
+        sampled_pts2 = pts2[indices]
+
+        # Compute the fundamental matrix using these 8 points
+        F = FindFundamentalMatrix(sampled_pts1, sampled_pts2)
+
+        # Count the number of inliers
+        inliers_count = 0
+        for i in range(len(pts1)):
+            pt1 = np.append(pts1[i], 1)  # homogeneous coordinates
+            pt2 = np.append(pts2[i], 1)
+
+            # Compute the distance from the point to the epipolar line
+            line = F @ pt1
+            distance = abs(pt2 @ line) / np.linalg.norm(line[:2])
+
+            # Check if the distance is below the threshold
+            if distance < threshold:
+                inliers_count += 1
+
+        # Update the best fundamental matrix if needed
+        if inliers_count > best_inliers_count:
+            best_inliers_count = inliers_count
+            best_fundamental_matrix = F
+
+    return best_fundamental_matrix
 
 if __name__ == '__main__':
     #Set parameters
     data_path = './data'
-    use_ransac = False
+    use_ransac = True
 
     #Load images
     image1_path = os.path.join(data_path, 'myleft.jpg')
@@ -89,7 +171,7 @@ if __name__ == '__main__':
         F = FindFundamentalMatrix(pts1, pts2)
 
     # Find epilines corresponding to points in second image,  and draw the lines on first image
-    lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F_true)
+    lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F_true)  # replace F_true with F to test the performance of our algorithm
     lines1 = lines1.reshape(-1, 3)
     img1, img2 = drawlines(image1, image2, lines1, pts1, pts2)
     fig, axis = plt.subplots(1, 2)
@@ -105,7 +187,7 @@ if __name__ == '__main__':
 
 
     # Find epilines corresponding to points in first image, and draw the lines on second image
-    lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F_true)
+    lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F_true)  # replace F_true with F to test the performance of our algorithm
     lines2 = lines2.reshape(-1, 3)
     img1, img2 = drawlines(image2, image1, lines2, pts2, pts1)
     fig, axis = plt.subplots(1, 2)
---
+++ lab4/deploy.py
@@ -0,0 +1,73 @@
+import cv2
+import torch
+import torchvision.transforms as transforms
+import time
+import sys
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Load the scripted model
+model = torch.jit.load(sys.argv[1])
+model.eval()
+
+square_crop_size = 1080
+
+# Define transformations
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.CenterCrop(square_crop_size),  # Center crop to a square
+    transforms.Resize((96, 96)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Initialize webcam
+cap = cv2.VideoCapture(0)
+
+prev_frame_time = 0
+new_frame_time = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Crop the frame to a square
+    height, width, _ = frame.shape
+    if height > width:
+        frame = frame[(height - width) // 2:(height + width) // 2, :]
+    else:
+        frame = frame[:, (width - height) // 2:(width + height) // 2]
+
+    # Time when we finish processing for this frame
+    new_frame_time = time.time()
+
+    # Preprocess the frame
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_tensor = transform(frame).unsqueeze(0)
+
+    # Model prediction
+    with torch.no_grad():
+        outputs = model(frame_tensor)
+        _, predicted = torch.max(outputs, 1)
+
+    # Add prediction text to frame
+    text = 'Face' if predicted.item() == 1 else 'No Face'
+    cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    # Calculate FPS
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    fps_text = f'FPS: {int(fps)}'
+    cv2.putText(frame, fps_text, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    # Display the resulting frame
+    cv2.imshow('Webcam', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    
+    # Break the loop with 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
